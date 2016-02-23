40eb16e87cd4477a3355c4b2e0e9e08c93bd0330
J. Michael Brewer <james.m.brewer@gmail.com>
2016-02-23 18:04:17 -0500

    Initial Commit

---
+++ src/VendingMachine.java
@@ -0,0 +1,10 @@
+package src;
+
+/**
+ * @author J. Michael Brewer
+ *
+ */
+
+class VendingMachine {
+
+}
---
+++ test/VendingMachineTest.java
@@ -0,0 +1,12 @@
+package test;
+/**
+ * Unit Tests for the Vending Machin Class
+ */ 
+
+/**
+ * @author J. Michael Brewer
+ *
+ */
+public class VendingMachineTest {
+
+}
